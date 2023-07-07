b9d9c97b8eb4d52e9038a4e41956c02737b905bf
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-07-07 18:26:15 +0530

    New Class Created

---
+++ ERP_HybridFrameWork/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Demo {
+	public static void add()
+	{
+		int a=75676,b=6,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	public static void div()
+	{
+		int a=590,b=4,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	public static void mul()
+	{
+		int a=45,b=8,c;
+		c=a+b;
+		System.out.println(c);
+	}
+}
--- ERP_HybridFrameWork/src/test/java/commonFunctions/FunctionLibrary.java
+++ ERP_HybridFrameWork/src/test/java/commonFunctions/FunctionLibrary.java
@@ -206,24 +206,7 @@ public class FunctionLibrary {
 		return df.format(date);
 				
 	}
-	public static void add()
-	{
-		int a=75676,b=6,c;
-		c=a+b;
-		System.out.println(c);
-	}
-	public static void div()
-	{
-		int a=590,b=4,c;
-		c=a+b;
-		System.out.println(c);
-	}
-	public static void mul()
-	{
-		int a=45,b=8,c;
-		c=a+b;
-		System.out.println(c);
-	}
+	
 }
 
 
